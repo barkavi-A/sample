9e02852ca466a001911ed1395e31b5d2b82b053c
barkavi-A <brkavi2006@gmail.com>
2025-02-18 20:48:31 +0530

    day3 task

---
+++ day 3/data.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+public class data {
+    public static void main(String args[]){
+        Scanner scan=new Scanner(System.in);
+        System.out.print("enter a:");
+        int a=scan.nextInt();
+        System.out.print("enter b:");
+        int b=scan.nextInt();
+        System.out.print("sum="+(a+b));
+       
+
+       
+
+
+    
+
+    }
+}
