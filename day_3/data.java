8f87d93bf25f7bdd5589a4a582f708e04cd3ad94
barkavi-A <brkavi2006@gmail.com>
2026-02-14 15:21:10 +0530

    new file adding

---
+++ day_3/data.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+public class data {
+    public static void main(String args[]){
+        Scanner scan=new Scanner(System.in);
+        System.out.print("enter c:");
+        int c=scan.nextInt();
+        System.out.print("enter b:");
+        int b=scan.nextInt();
+        System.out.print("sum="+(c+b));
+       
+
+       
+
+
+    
+
+    }
+}
